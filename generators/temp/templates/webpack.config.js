7e31a90b2a2def8772a856041fe965e51ae9e7ac
Charlie Jackson <charlie@charliejackson.com>
2017-03-09 11:39:12 +0000

    Added sourcemap support in uglify.js

--- generators/temp/templates/webpack.config.js
+++ generators/temp/templates/webpack.config.js
@@ -63,6 +63,7 @@ module.exports = (env = {}) => {
       (() => {
         if (isProduction) {
           return new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
             beautify: false,
             mangle: {
               screw_ie8: true,
